b639f7f163ed951c44ea61309456d9bea9595deb
annaleventon25 <94017620+annaleventon25@users.noreply.github.com>
2024-01-07 22:21:30 -0500

    changing color

--- CreativeCoding/P5/BackgroundColor/sketch.js
+++ CreativeCoding/P5/BackgroundColor/sketch.js
@@ -1,6 +1,6 @@
 function setup() {
 createCanvas (windowWidth, windowHeight);//this creates the window width and height to be the size of my browser window
-background (196,196,196);//light gray background
+background (250,133,255);//light pink background
 }
 
 function draw() {
