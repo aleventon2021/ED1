f930b17cce03fadf18e4fb3904dc0a5589f11dc2
annaleventon25 <94017620+annaleventon25@users.noreply.github.com>
2024-01-07 22:19:19 -0500

    pushing p5 to main

---
+++ CreativeCoding/P5/BackgroundColor/sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+createCanvas (windowWidth, windowHeight);//this creates the window width and height to be the size of my browser window
+background (196,196,196);//light gray background
+}
+
+function draw() {
+
+}
