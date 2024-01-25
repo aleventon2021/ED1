7e2ccaad03bd2be12f5a18abed595ac80b2486dc
annaleventon25 <94017620+annaleventon25@users.noreply.github.com>
2024-01-25 10:45:35 -0500

    update webpage

---
+++ sketch.js
@@ -0,0 +1,32 @@
+let gif;
+
+function preload (){
+  gif = loadImage('assets/ALeventon_Final copy.gif');
+}
+
+
+
+function setup() {//runs once
+  createCanvas(windowWidth, windowHeight);
+  fill('lightblue');
+  background ('lightgray');
+  strokeWeight(1);
+}
+
+function draw() {//runs in a loop
+
+  var num = 8;//variable for the number of squares
+  var sideLen = windowWidth/num;//variable for the side length of each square
+
+  for (var y = 0; y<windowWidth; y=y+sideLen){//loop to create a row of squares in the y direction
+
+  for (var x=0; x<windowWidth; x=x+sideLen){//loop to create a row of squares in the x direction
+  image(gif, x, y, sideLen, sideLen);
+  }
+  }
+
+}
+
+function windowResized (){
+  resizeCanvas (windowWidth, windowHeight);
+}
