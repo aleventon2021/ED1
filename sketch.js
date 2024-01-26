b2e450ff1ce468e1e53bc691fd259dffd3be3d2f
annaleventon25 <94017620+annaleventon25@users.noreply.github.com>
2024-01-26 13:00:45 -0500

    updating

--- sketch.js
+++ sketch.js
@@ -18,9 +18,9 @@ function draw() {//runs in a loop
   var num = 8;//variable for the number of squares
   var sideLen = windowWidth/num;//variable for the side length of each square
 
-  for (var y = 0; y<windowWidth; y=y+sideLen){//loop to create a row of squares in the y direction
+  for (var y = 0; y< 2*windowWidth; y=y+sideLen){//loop to create a row of squares in the y direction
 
-  for (var x=0; x<windowWidth; x=x+sideLen){//loop to create a row of squares in the x direction
+  for (var x=0; x< 2*windowWidth; x=x+sideLen){//loop to create a row of squares in the x direction
   image(gif, x, y, sideLen, sideLen);
   }
   }
